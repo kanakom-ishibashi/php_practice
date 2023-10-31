4c4e84047c9b9cbb08dcf9f6b21b70da03622ea5
kanakom-ishibashi <kanakom001@gmail.com>
2023-10-31 09:55:09 +0000

    kadai04.php

--- kadai04.php
+++ kadai04.php
@@ -5,7 +5,7 @@ $b = 7;
 echo $a + $b;
 echo "\n";
 // 課題2
-$array_month = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+$array_month = ["1月", 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 $array_month [5];
 echo "\n";
 
