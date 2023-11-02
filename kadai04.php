191bf9daa59c07030faef457fb4593fe50aca182
kanakom-ishibashi <kanakom001@gmail.com>
2023-11-02 08:03:40 +0000

    kadai04.php

--- kadai04.php
+++ kadai04.php
@@ -4,8 +4,24 @@ $a = 3;
 $b = 7;
 echo $a + $b;
 echo "\n";
+
 // 課題2
-$array_month = ["1月", 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-$array_month [5];
+
+$array_month = ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"];
+echo $array_month[7];
 echo "\n";
+
+// 課題3
+$hello = "Hello,";
+$name = "kanako";
+$world = "'s World!";
+echo $hello . $name . $world; 
+echo "\n";
+
+// 課題4
+$tech_boost = "tech";
+$tech_boost .= " boost";
+echo $tech_boost;
+echo "\n";
+
 
