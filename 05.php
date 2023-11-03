bb137acdab7d9d75823004b61f5fb56219b6fc92
kanakom-ishibashi <kanakom001@gmail.com>
2023-11-03 07:51:37 +0000

    initial commit

--- 05.php
+++ 05.php
@@ -1,4 +1,5 @@
 <?php
+// 課題1
 $name ="村松";
 if ($name = "村松") {
   echo "私は 村松です。";
@@ -6,4 +7,26 @@ if ($name = "村松") {
 else {
   echo "私の名前ではありません。";
 } 
+echo "\n";
+
+// 課題2
+$sum = 0;
+for ($i = 1; $i <= 10000; $i++) {
+  $sum += $i;
+}
+echo "1から10000までの合計は: " . $sum;
+echo "\n";
+
+// 課題3
+$fruits = array("apple", "orange", "lemon", "meron", "peach");
+echo count($fruits);
+for ($i = 0; $i < count($fruits); $i++) {
+  echo "要素は" . $fruits[$i];
+  echo "\n";
+}
+foreach($fruits as $fruits){
+  echo "要素は" . $fruits;
+  echo "\n";
+}
+
 ?>
